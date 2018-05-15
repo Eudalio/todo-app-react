bdf1830b154496a563b4f4939bc56c412bb49cdf
Eudalio <tonnydallios@gmail.com>
2018-05-15 13:50:30 -0300

    commit de mudança de maquina - (problem importing the preset es2015)

---
+++ frontend/src/main/app.jsx
@@ -0,0 +1,11 @@
+import 'modules/bootstrap/dist/css/bootstrap.min.css'
+import 'modules/font-awesome/css/font-awesome.min.css'
+
+import React from 'react'
+
+export default props => {
+    //class no html no react vira className
+    <div className='container'>
+        <h1>Teste</h1>
+    </div>
+}
