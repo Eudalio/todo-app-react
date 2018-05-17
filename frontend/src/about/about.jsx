d425004570b414e3fb1528f4ecc57247d11e6b28
eudalio <tonnydallios@gmail.com>
2018-05-17 00:11:37 -0300

    added pageHeader

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <h2>Nossa história</h2>
+        <h2>Missão</h2>
+        <h2>Imprensa</h2>
     </div>
 )
