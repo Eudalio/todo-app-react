675c68f28b844cd62d7c952271ca14d182e0f5b2
eudalio <tonnydallios@gmail.com>
2018-07-02 08:58:32 -0300

    frontend without redux

---
+++ frontend-sem-redux/src/about/about.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Tarefas' small='Sobre'/>
+        <h2>Está página tem apenas o intuito de testar a navegação.</h2>
+    </div>
+)
