3b0ecc8a0e75ab4ce434e8dc13361f3c1e9f6534
eudalio <tonnydallios@gmail.com>
2018-07-09 08:43:50 -0300

    Aplicando Middleware (Redux-promise)

--- frontend/src/todo/todoReducer.js
+++ frontend/src/todo/todoReducer.js
@@ -1,21 +1,4 @@
-const INITIAL_STATE = {
-    description: 'Ler livro',
-    list: [{
-        _id: 1,
-        description: 'Pagar conta do cartão',
-        done: true
-    },
-    {
-        _id: 2,
-        description: 'Read about react native',
-        done: false
-    },
-    {
-        _id: 3,
-        description: 'Estudar Inglês',
-        done: false
-    }]
-}
+const INITIAL_STATE = { description: '', list: [] }
 
 export default (state = INITIAL_STATE, action) => {
     switch(action.type) {
